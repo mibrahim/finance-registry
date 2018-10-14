c7b0c8c7334d862c0b997c198f9aa4ae750e0d84
Mohamed Ibrahim <mibrahim@mibrahim.net>
2018-10-14 09:27:08 -0400

    Fix configs

--- conf/config.sh
+++ conf/config.sh
@@ -8,7 +8,7 @@ locale-gen en_US.UTF-8
 
 cp /conf/apachesites/site-md.conf /etc/apache2/sites-available/000-default.conf
 cp /conf/apachesites/apache2.conf /etc/apache2/apache2.conf
-cp /conf/phpconf/php.ini /etc/php/7.0/apache2/php.ini
+cp /conf/phpconf/php.ini /etc/php/7.2/apache2/php.ini
 
 # Enable mod rewrite
 ln -s /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
--- envprep.sh
+++ envprep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Install openssh server, less, sudo, tmux, rsync
-LIBS="locales dialog vim screen bash-completion openssh-server less sudo tmux rsync sendmail"
+LIBS="locales dialog vim screen bash-completion openssh-server less sudo sendmail"
 
 # Install postgresql
 LIBS="$LIBS postgresql-client"
@@ -9,4 +9,9 @@ LIBS="$LIBS postgresql-client"
 # Install apache and php
 LIBS="$LIBS apache2 libapache2-mod-php7.2 apache2-utils php php7.2 php7.2-xml php7.2-pgsql php7.2-cli php-pear"
 
-apt-get update && apt-get install -y $LIBS && apt-get clean && rm -rf /var/lib/apt/lists/*
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y $LIBS
+apt-get clean
+rm -rf /var/lib/apt/lists/*
