d49e00ff095e1f23229e4221cb194e553f38a921
Mohamed Ibrahim <mibrahim@mibrahim.net>
2019-03-09 23:49:33 -0500

    Center the table

--- web/p_and_l.php
+++ web/p_and_l.php
@@ -21,10 +21,11 @@ $result = query("select target, sum(amount) as sum from txns $filter group by ta
 
 $Page['contents'] .= '
 <br/><br/>
+<center>
 <table class="table table-hover" style="width:max-content;">
 <thead>
     <tr class="thead-dark">
-        <th>Target</th>
+        <th>Target Account</th>
         <th>Total</th>
     </tr>
 </thead>
@@ -45,10 +46,10 @@ while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
 
 foreach ($sum as $key => $value) {
     $spaces = "";
-    for($i=0;$i!=substr_count($key,":");$i++) $spaces.="&nbsp;";
+    for($i=0;$i!=substr_count($key,":");$i++) $spaces.="&nbsp;&nbsp;&nbsp;&nbsp;";
     $Page['contents'] .= "<tr><td><code>$spaces $key</code></td><td><code>" . formatNumber($value) . "</code></td></tr>";
 }
 
-$Page['contents'] .= "</table>";
+$Page['contents'] .= "</table></center>";
 
 include dirname(__FILE__) . "/templates/responsive.php";
