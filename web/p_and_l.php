cb1d41ec42071e0e7599238fe2bf80c345edfa26
Mohamed Ibrahim <mibrahim@mibrahim.net>
2019-03-09 23:38:25 -0500

    Basic P&L

---
+++ web/p_and_l.php
@@ -0,0 +1,54 @@
+<?php
+require_once dirname(__FILE__) . "/inc/conf.php";
+require_once dirname(__FILE__) . "/inc/utils.php";
+
+include "topbar.php";
+
+$filter = "";
+
+if ($entity != null) {
+    $filter .= "entity = '" . se($entity) . "' ";
+}
+
+if ($account != null) {
+    if (strlen($filter)) $filter .= " and ";
+    $filter .= "account = '" . se($account) . "' ";
+}
+
+if (strlen($filter)) $filter = " where $filter ";
+
+$result = query("select target, sum(amount) as sum from txns $filter group by target order by target");
+
+$Page['contents'] .= '
+<br/><br/>
+<table class="table table-hover" style="width:max-content;">
+<thead>
+    <tr class="thead-dark">
+        <th>Target</th>
+        <th>Total</th>
+    </tr>
+</thead>
+';
+
+$sum = array();
+
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+    $parts = explode(":", $row['target']);
+    $connected = "";
+    for ($i = 0; $i != sizeof($parts); $i++) {
+        if (strlen($connected) > 0) $connected .= ":";
+        $connected .= $parts[$i];
+        if (!isset($sum[$connected])) $sum[$connected] = $row['sum'];
+        else $sum[$connected] += $row['sum'];
+    }
+}
+
+foreach ($sum as $key => $value) {
+    $spaces = "";
+    for($i=0;$i!=substr_count($key,":");$i++) $spaces.="&nbsp;";
+    $Page['contents'] .= "<tr><td><code>$spaces $key</code></td><td><code>" . formatNumber($value) . "</code></td></tr>";
+}
+
+$Page['contents'] .= "</table>";
+
+include dirname(__FILE__) . "/templates/responsive.php";
