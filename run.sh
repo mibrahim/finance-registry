46819b74cdbd355377f41c4a3afb8a040b86971e
Mohamed Ibrahim <mibrahim@mibrahim.net>
2019-06-23 09:35:54 -0400

    Pull the latest image

--- run.sh
+++ run.sh
@@ -12,4 +12,5 @@ esac
 
 ./stop.sh
 ${SUDO} docker rm --force multidate-app
+${SUDO} docker pull m0brhm/php-apache
 ${SUDO} docker run -d -p 8123:80 --name multidate-app -v "$PWD/web/":/var/www/html php:7.2-apache
